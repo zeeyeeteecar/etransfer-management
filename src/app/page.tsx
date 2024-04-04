a9677d732caf75cab1824e78044ac5a9e2716274
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-04-04 16:55:09 -0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function Page() {
   const router = useRouter();
-  router.push("/login");
+
+  React.useEffect(() => {
+    router.push("/login");
+  }, []);
 
   return <></>;
 }
